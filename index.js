e6841f0b52f2e795491ca2de1d86955a43b81e44
Bryzzen <xdbrgames10@gmail.com>
2023-03-23 16:19:05 -0300

    Alpha Finalizada

--- index.js
+++ index.js
@@ -15,9 +15,46 @@ manager.addDocument('pt', 'Como faço para acessar o portal do aluno', 'portal')
 manager.addDocument('pt', 'Como faço para acessar o portal de educação', 'portal');
 manager.addDocument('pt', 'Como faço para acessar o portal sesi', 'portal');
 manager.addDocument('pt', 'Como faço para acessar o portal do aluno sesi', 'portal');
+manager.addDocument('pt', 'Como faço para acessar o portal do aluno do sesi', 'portal');
+manager.addDocument('pt', "Como posso fazer para usar o canva", "canva");
+manager.addDocument('pt', 'Alvaro', 'alvaro');
+manager.addAnswer('pt', 'alvaro', 'Alvaro é o melhor professor de todos os tempos, ele é muito legal e sabe tudo sobre programação.');
 manager.addAnswer('pt', 'cumprimento', 'Olá, em que posso ajudar?');
 manager.addAnswer('pt', 'nome', 'Meu nome é Chatbot, sou um assistente virtual.');
-manager.addAnswer('pt', 'portal', 'Para acessar o portal do aluno, basta clicar no link: https://portal.sesi.org.br/');
+manager.addAnswer('pt', 'portal', 'Para acessar o portal do aluno, basta acessar o link: https://portal.sesi.org.br/');
+manager.addAnswer('pt', 'canva', 'Para utilizar o Canva, basta acessar o site ou baixar o aplicativo, criar uma conta ou fazer login, e escolher um modelo ou começar um projeto do zero utilizando as ferramentas disponíveis na plataforma.');
+
+manager.addDocument('pt', 'O que é inteligência artificial?', 'ia');
+manager.addDocument('pt', 'Qual a definição de inteligência artificial?', 'ia');
+manager.addDocument('pt', 'Como funciona a inteligência artificial?', 'ia');
+manager.addDocument('pt', 'Qual é a diferença entre machine learning e inteligência artificial?', 'ia');
+
+manager.addAnswer('pt', 'ia', 'A inteligência artificial é um campo da ciência da computação que busca criar máquinas capazes de executar tarefas que normalmente exigem inteligência humana, como aprendizado, resolução de problemas e reconhecimento de padrões. Machine learning é uma técnica utilizada na inteligência artificial para permitir que as máquinas aprendam com os dados sem serem explicitamente programadas.');
+
+manager.addDocument('pt', 'O que é deep learning?', 'deep-learning');
+manager.addDocument('pt', 'Como funciona o deep learning?', 'deep-learning');
+manager.addDocument('pt', 'Qual a diferença entre machine learning e deep learning?', 'deep-learning');
+
+manager.addAnswer('pt', 'deep-learning', 'Deep learning é uma técnica de aprendizado de máquina que utiliza redes neurais artificiais com várias camadas de processamento para aprender representações de dados complexos. É uma forma avançada de machine learning que pode ser usada para reconhecimento de fala, visão computacional e processamento de linguagem natural.');
+
+manager.addDocument('pt', 'O que é big data?', 'big-data');
+manager.addDocument('pt', 'Qual a definição de big data?', 'big-data');
+manager.addDocument('pt', 'Como funciona o big data?', 'big-data');
+
+manager.addDocument('pt', 'Como faço para ver minhas notas no portal do aluno?', 'notas');
+manager.addDocument('pt', 'Onde posso encontrar minhas notas no portal do aluno?', 'notas');
+manager.addDocument('pt', 'Qual é o procedimento para acessar minhas notas no portal do aluno?', 'notas');
+manager.addAnswer('pt', 'notas', 'Para ver suas notas no portal do aluno do SESI, faça login na plataforma com suas credenciais e clique na opção "Notas" no menu lateral. Lá você encontrará todas as suas notas e o histórico de desempenho nas disciplinas.');
+
+manager.addDocument('pt', 'Como posso ver minha grade de horário no portal do aluno?', 'horario');
+manager.addDocument('pt', 'Onde encontro minha grade de horário no portal do aluno?', 'horario');
+manager.addDocument('pt', 'Qual é o procedimento para acessar minha grade de horário no portal do aluno?', 'horario');
+manager.addAnswer('pt', 'horario', 'Para ver sua grade de horário no portal do aluno do SESI, faça login na plataforma com suas credenciais e clique na opção "Grade Horária" no menu lateral. Lá você encontrará todas as informações sobre seus horários de aula, turmas e disciplinas.');
+
+manager.addDocument('pt', 'O que é uma incubadora de startups?', 'incubadora');
+manager.addDocument('pt', 'Como funciona uma incubadora de startups?', 'incubadora');
+manager.addDocument('pt', 'Quais são os benefícios de participar de uma incubadora de startups?', 'incubadora');
+
 
 
 app.get('/', (req, res) => {
