0e18dbf69e1cf7c60b14f6d8250d7a4d52529932
Bryzzen <xdbrgames10@gmail.com>
2023-03-27 16:06:25 -0300

    Erros de espaçamento resolvido

--- index.js
+++ index.js
@@ -46,8 +46,7 @@ manager.addAnswer(
 manager.addAnswer(
     "pt",
     "portal",
-    `
-    Para acessar o site SESI Educação, siga as instruções abaixo:
+    `Para acessar o site SESI Educação, siga as instruções abaixo:
 1.	Abra o navegador da web em seu computador, tablet ou smartphone.
 2.	Digite "https://www.sesieducacao.com.br/" na barra de endereços do seu navegador.
 3.	Pressione a tecla "Enter" para carregar a página inicial do site SESI Educação.
@@ -56,8 +55,7 @@ Na página inicial do site SESI Educação, você encontrará informações sobr
 manager.addAnswer(
     "pt",
     "canva",
-    `
-    Para utilizar o Canva, siga estes passos:
+    `Para utilizar o Canva, siga estes passos:
 1.	Acesse o www.canva.com e crie uma conta gratuita.
 2.	Após a inscrição, você será levado a página inicial, onde poderá criar e personalizar seu design.
 3.	O Canva possui milhares de modelos gratuitos e pagos que você pode escolher.
@@ -67,8 +65,7 @@ manager.addAnswer(
 
 manager.addDocument("pt", "microsoft teams", "teams");
 
-manager.addAnswer('pt', 'teams', `
-O Microsoft Teams é uma plataforma de comunicação, que pode ser usada para comunicar-se  com colegas de trabalho, amigos e familiares. Para ser utilizado, siga estas etapas:
+manager.addAnswer('pt', 'teams', `O Microsoft Teams é uma plataforma de comunicação, que pode ser usada para comunicar-se  com colegas de trabalho, amigos e familiares. Para ser utilizado, siga estas etapas:
 1.Crie uma conta usando um endereço de e-mail existente ou criando uma nova
 2. Faça o download do aplicativo em https://www.microsoft.com/teams/download-app.
 3.Depois de baixado e instalado, entre na sua conta usando seu login.
@@ -79,8 +76,7 @@ Essas são as etapas básicas  para ser usado, á medida que você se torna mais
 manager.addDocument('pt', 'powerpoint', 'powerpoint')
 manager.addDocument('pt', 'power point', 'powerpoint')
 
-manager.addAnswer('pt', 'powerpoint', `
-Para começar a usar o PowerPoint, siga os seguintes passos:
+manager.addAnswer('pt', 'powerpoint', `Para começar a usar o PowerPoint, siga os seguintes passos:
 1.	Abra o PowerPoint em seu computador. Você pode encontrá-lo em seu menu de aplicativos ou na área de trabalho.
 2.	Na tela inicial do PowerPoint, clique em "Novapresentação" para criar uma nova apresentação em branco. Alternativamente, você pode escolher um modelo de apresentação pré-existente que se encaixe no seu projeto.
 3.	Na aba "Inserir", você pode adicionar slides, imagens, vídeos, gráficos, tabelas e muito mais ao seu projeto. Você também pode escolher um design para seus slides na aba "Design".
@@ -92,8 +88,7 @@ Essas são apenas algumas das principais funções do PowerPoint
 
 manager.addDocument('pt', 'ava', 'ava')
 
-manager.addAnswer('pt', 'ava', `
-O AVA, que significa Ambiente Virtual de Aprendizagem, é uma plataforma online que permite a realização de atividades acadêmicas e o acompanhamento do progresso do estudante em um curso ou disciplina
+manager.addAnswer('pt', 'ava', `O AVA, que significa Ambiente Virtual de Aprendizagem, é uma plataforma online que permite a realização de atividades acadêmicas e o acompanhamento do progresso do estudante em um curso ou disciplina
 1.	Acesse a plataforma: O acesso ao é geralmente feito através do site da instituição de ensino ou por meio de um link fornecido pelo professor. Certifique-se de ter as informações de login e senha corretas.
 2.	O AVA pode oferecer diversas funcionalidades, como acesso a materiais de estudo, fóruns de discussão, chat com professores e colegas, realização de testes e trabalhos, entre outras
 3.	Realize as avaliações: As avaliações no AVA podem ser em formato de testes online, trabalhos ou outras atividades. Certifique-se de entender bem as instruções e os critérios de avaliação para realizar as tarefas da melhor maneira possível.
@@ -122,8 +117,7 @@ manager.addDocument("pt", 'office', 'office')
 manager.addDocument('pt', 'microsoft office', 'office')
 manager.addDocument('pt', 'office 365', 'office')
 
-manager.addAnswer('pt', 'office', `
-O Office 365 é uma suíte de aplicativos de produtividade da Microsoft que oferece uma variedade de ferramentas para criação e edição de documentos, planilhas, apresentações e muito mais. Para usar o Office 365, siga as instruções abaixo:
+manager.addAnswer('pt', 'office', `O Office 365 é uma suíte de aplicativos de produtividade da Microsoft que oferece uma variedade de ferramentas para criação e edição de documentos, planilhas, apresentações e muito mais. Para usar o Office 365, siga as instruções abaixo:
 1.	Acesse o site do Office 365 em https://www.office.com/ e faça login com sua conta Microsoft.
 2.	Na página inicial do Office 365, você verá todos os aplicativos disponíveis, como Word, Excel, PowerPoint, Outlook, OneDrive, Teams, entre outros. Clique em um aplicativo para abri-lo.
 3.	Quando o aplicativo for aberto, você poderá criar um novo documento ou abrir um existente. Para criar um novo documento, clique no botão "Novo" e escolha o tipo de documento que deseja criar.
@@ -151,8 +145,7 @@ manager.addAnswer(
 manager.addDocument("pt", "portal sesi", "portal");
 
 manager.addDocument("pt", "sesi", 'sesi')
-manager.addAnswer('pt', 'sesi', `
-O Serviço Social da Indústria (SESI) está com matrículas abertas para o ano letivo de 2022. Atualmente, a rede possui quase 500 escolas espalhadas por todo Brasil e é referência nacional em educação e no preparo dos estudantes para os desafios do mundo do trabalho. Não perca essa oportunidade de começar o seu futuro!
+manager.addAnswer('pt', 'sesi', `O Serviço Social da Indústria (SESI) está com matrículas abertas para o ano letivo de 2022. Atualmente, a rede possui quase 500 escolas espalhadas por todo Brasil e é referência nacional em educação e no preparo dos estudantes para os desafios do mundo do trabalho. Não perca essa oportunidade de começar o seu futuro!
 `)
 
 manager.addDocument("pt", "O que é big data?", "big-data");
@@ -164,8 +157,7 @@ manager.addDocument("pt", "codigofonte", "codigo-fonte");
 
 manager.addDocument("pt", "sourcecode", "codigo-fonte");
 
-manager.addAnswer('pt', 'codigo-fonte', `
-O meu código fonte está disponível no meu github:
+manager.addAnswer('pt', 'codigo-fonte', `O meu código fonte está disponível no meu github:
 https://github.com/bryzzen-kibador/Assistente-Virtual-SESI
 `)
 
