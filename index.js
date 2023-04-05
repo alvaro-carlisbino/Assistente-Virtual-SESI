5acd463eecb042a79746f6965c62c9f9ac471910
Bryzzen <xdbrgames10@gmail.com>
2023-04-05 14:25:50 -0300

    Introdução adicionada, licença adicionada

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const io = require("socket.io")(http);
 const port = process.env.PORT || 3000;
 const { NlpManager } = require("node-nlp");
 const manager = new NlpManager({ languages: ["pt"] });
-require("ejs")
+require("ejs");
 app.set("view engine", "ejs");
 
 manager.addDocument("pt", "Olá", "cumprimento");
@@ -35,7 +35,7 @@ manager.addDocument("pt", "Alvaro", "alvaro");
 manager.addAnswer(
     "pt",
     "alvaro",
-    "Alvaro é o melhor professor de todos os tempos, ele é muito legal e sabe tudo sobre programação."
+    "Alvaro é o melhor profissional de todos os tempos, ele é muito legal e sabe tudo sobre programação."
 );
 manager.addAnswer("pt", "cumprimento", "Olá, em que posso ajudar?");
 manager.addAnswer(
@@ -65,18 +65,25 @@ manager.addAnswer(
 
 manager.addDocument("pt", "microsoft teams", "teams");
 
-manager.addAnswer('pt', 'teams', `O Microsoft Teams é uma plataforma de comunicação, que pode ser usada para comunicar-se  com colegas de trabalho, amigos e familiares. Para ser utilizado, siga estas etapas:
+manager.addAnswer(
+    "pt",
+    "teams",
+    `O Microsoft Teams é uma plataforma de comunicação, que pode ser usada para comunicar-se  com colegas de trabalho, amigos e familiares. Para ser utilizado, siga estas etapas:
 1.Crie uma conta usando um endereço de e-mail existente ou criando uma nova
 2. Faça o download do aplicativo em https://www.microsoft.com/teams/download-app.
 3.Depois de baixado e instalado, entre na sua conta usando seu login.
 4. O Microsoft Teams permite que você compartilhe arquivos e trabalhe em projetos em equipe. Você pode criar canais específicos para projetos, compartilhar arquivos e colaborar em tempo real.
 Essas são as etapas básicas  para ser usado, á medida que você se torna mais confortável com a plataforma, pode explorar recursos adicionais
-`)
+`
+);
 
-manager.addDocument('pt', 'powerpoint', 'powerpoint')
-manager.addDocument('pt', 'power point', 'powerpoint')
+manager.addDocument("pt", "powerpoint", "powerpoint");
+manager.addDocument("pt", "power point", "powerpoint");
 
-manager.addAnswer('pt', 'powerpoint', `Para começar a usar o PowerPoint, siga os seguintes passos:
+manager.addAnswer(
+    "pt",
+    "powerpoint",
+    `Para começar a usar o PowerPoint, siga os seguintes passos:
 1.	Abra o PowerPoint em seu computador. Você pode encontrá-lo em seu menu de aplicativos ou na área de trabalho.
 2.	Na tela inicial do PowerPoint, clique em "Novapresentação" para criar uma nova apresentação em branco. Alternativamente, você pode escolher um modelo de apresentação pré-existente que se encaixe no seu projeto.
 3.	Na aba "Inserir", você pode adicionar slides, imagens, vídeos, gráficos, tabelas e muito mais ao seu projeto. Você também pode escolher um design para seus slides na aba "Design".
@@ -84,19 +91,22 @@ manager.addAnswer('pt', 'powerpoint', `Para começar a usar o PowerPoint, siga o
 5.	Na aba "Apresentação de Slides", você pode iniciar a apresentação em tela cheia para ver como ela ficará.
 6.	Salve sua apresentação regularmente. Você pode fazer isso clicando em "Arquivo" e depois em "Salvar" ou pressionando Ctrl + S.
 Essas são apenas algumas das principais funções do PowerPoint
-`)
+`
+);
 
-manager.addDocument('pt', 'ava', 'ava')
+manager.addDocument("pt", "ava", "ava");
 
-manager.addAnswer('pt', 'ava', `O AVA, que significa Ambiente Virtual de Aprendizagem, é uma plataforma online que permite a realização de atividades acadêmicas e o acompanhamento do progresso do estudante em um curso ou disciplina
+manager.addAnswer(
+    "pt",
+    "ava",
+    `O AVA, que significa Ambiente Virtual de Aprendizagem, é uma plataforma online que permite a realização de atividades acadêmicas e o acompanhamento do progresso do estudante em um curso ou disciplina
 1.	Acesse a plataforma: O acesso ao é geralmente feito através do site da instituição de ensino ou por meio de um link fornecido pelo professor. Certifique-se de ter as informações de login e senha corretas.
 2.	O AVA pode oferecer diversas funcionalidades, como acesso a materiais de estudo, fóruns de discussão, chat com professores e colegas, realização de testes e trabalhos, entre outras
 3.	Realize as avaliações: As avaliações no AVA podem ser em formato de testes online, trabalhos ou outras atividades. Certifique-se de entender bem as instruções e os critérios de avaliação para realizar as tarefas da melhor maneira possível.
 
 Esses é o processo básico para acessar o Ava e conseguir concluir as avaliações
-`)
-
-
+`
+);
 
 manager.addDocument("pt", "O que é inteligência artificial?", "ia");
 manager.addDocument("pt", "Qual a definição de inteligência artificial?", "ia");
@@ -113,11 +123,14 @@ manager.addAnswer(
     "A inteligência artificial é um campo da ciência da computação que busca criar máquinas capazes de executar tarefas que normalmente exigem inteligência humana, como aprendizado, resolução de problemas e reconhecimento de padrões. Machine learning é uma técnica utilizada na inteligência artificial para permitir que as máquinas aprendam com os dados sem serem explicitamente programadas."
 );
 
-manager.addDocument("pt", 'office', 'office')
-manager.addDocument('pt', 'microsoft office', 'office')
-manager.addDocument('pt', 'office 365', 'office')
+manager.addDocument("pt", "office", "office");
+manager.addDocument("pt", "microsoft office", "office");
+manager.addDocument("pt", "office 365", "office");
 
-manager.addAnswer('pt', 'office', `O Office 365 é uma suíte de aplicativos de produtividade da Microsoft que oferece uma variedade de ferramentas para criação e edição de documentos, planilhas, apresentações e muito mais. Para usar o Office 365, siga as instruções abaixo:
+manager.addAnswer(
+    "pt",
+    "office",
+    `O Office 365 é uma suíte de aplicativos de produtividade da Microsoft que oferece uma variedade de ferramentas para criação e edição de documentos, planilhas, apresentações e muito mais. Para usar o Office 365, siga as instruções abaixo:
 1.	Acesse o site do Office 365 em https://www.office.com/ e faça login com sua conta Microsoft.
 2.	Na página inicial do Office 365, você verá todos os aplicativos disponíveis, como Word, Excel, PowerPoint, Outlook, OneDrive, Teams, entre outros. Clique em um aplicativo para abri-lo.
 3.	Quando o aplicativo for aberto, você poderá criar um novo documento ou abrir um existente. Para criar um novo documento, clique no botão "Novo" e escolha o tipo de documento que deseja criar.
@@ -126,7 +139,8 @@ manager.addAnswer('pt', 'office', `O Office 365 é uma suíte de aplicativos de 
 6.	Para compartilhar um documento com outras pessoas, clique no botão "Compartilhar" e adicione as pessoas com quem deseja compartilhar o arquivo. Você pode conceder diferentes níveis de permissão, como visualização ou edição.
 
 Essas são as instruções básicas para a utilização do office 365.
-`)
+`
+);
 
 manager.addDocument("pt", "O que é deep learning?", "deep-learning");
 manager.addDocument("pt", "Como funciona o deep learning?", "deep-learning");
@@ -144,9 +158,13 @@ manager.addAnswer(
 
 manager.addDocument("pt", "portal sesi", "portal");
 
-manager.addDocument("pt", "sesi", 'sesi')
-manager.addAnswer('pt', 'sesi', `O Serviço Social da Indústria (SESI) está com matrículas abertas para o ano letivo de 2022. Atualmente, a rede possui quase 500 escolas espalhadas por todo Brasil e é referência nacional em educação e no preparo dos estudantes para os desafios do mundo do trabalho. Não perca essa oportunidade de começar o seu futuro!
-`)
+manager.addDocument("pt", "sesi", "sesi");
+manager.addAnswer(
+    "pt",
+    "sesi",
+    `O Serviço Social da Indústria (SESI) está com matrículas abertas para o ano letivo de 2022. Atualmente, a rede possui quase 500 escolas espalhadas por todo Brasil e é referência nacional em educação e no preparo dos estudantes para os desafios do mundo do trabalho. Não perca essa oportunidade de começar o seu futuro!
+`
+);
 
 manager.addDocument("pt", "O que é big data?", "big-data");
 manager.addDocument("pt", "Qual a definição de big data?", "big-data");
@@ -157,9 +175,13 @@ manager.addDocument("pt", "codigofonte", "codigo-fonte");
 
 manager.addDocument("pt", "sourcecode", "codigo-fonte");
 
-manager.addAnswer('pt', 'codigo-fonte', `O meu código fonte está disponível no meu github:
+manager.addAnswer(
+    "pt",
+    "codigo-fonte",
+    `O meu código fonte está disponível no meu github:
 https://github.com/bryzzen-kibador/Assistente-Virtual-SESI
-`)
+`
+);
 
 manager.addDocument(
     "pt",
@@ -217,15 +239,18 @@ manager.addDocument(
 
 let rooms = [];
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+    res.render(__dirname + "/index.ejs");
+})
+
+
+app.get("/chat", (req, res) => {
     let i = 0;
     while (rooms[i] != undefined) i++;
-    res.render(__dirname + "/index.ejs", { room: i });
+    res.render(__dirname + "/chat.ejs", { room: i });
 });
 
-
 io.on("connection", (socket) => {
-
     socket.on("join", (room) => {
         console.log(`Usuário ${socket.id} entrou na sala ${room}`);
         socket.join(room);
@@ -235,13 +260,17 @@ io.on("connection", (socket) => {
     socket.on("chat message", (data) => {
         manager.process("pt", data.message).then((response) => {
             if (response.answer == undefined) {
-                response.answer = "Desculpe, não entendi sua pergunta."
+                response.answer = "Desculpe, não entendi sua pergunta.";
             }
             io.to(data.room).emit("chat message", {
-                message: response.answer, sender: "robot"
+                message: response.answer,
+                sender: "robot",
             });
-        })
-        io.to(data.room).emit("chat message", { message: data.message, sender: "user" });
+        });
+        io.to(data.room).emit("chat message", {
+            message: data.message,
+            sender: "user",
+        });
     });
 
     socket.on("leave", (room) => {
@@ -250,7 +279,6 @@ io.on("connection", (socket) => {
         rooms.splice(rooms.indexOf(room), 1);
     });
 });
-
 
 http.listen(port, async () => {
     await manager.train();
